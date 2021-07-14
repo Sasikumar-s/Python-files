fcbe87dfb24a8bccc7e202c48a6b4af00cb65650
Sasikumar-s <sasikumarbe2018@gmail.com>
2021-07-14 15:56:52 +0530

    Chaged on lab

--- Lab/7-check_number_is_polindrome.py
+++ Lab/7-check_number_is_polindrome.py
@@ -1,8 +1,13 @@
 #To check number is polindrome
-l=[]
+
 a=int(input("Enter number"))
+temp = a
+rev=0
 while a>0:
-	q=a%10
-	l.append(q)
+	dig=a%10
+	rev = rev*10+dig
 	a=a//10
-print(''.join(l))
+if rev==temp:
+	print("Given number is palindrome :",temp)
+else:
+	print("Given number is not a palindrome :",temp)
---
+++ Lab/8-Convert temperature in celsius to fahenheit.py
@@ -0,0 +1,5 @@
+#Convert temperature in celsius to fahenheit
+
+temp = int(input("Enter temperature"))
+f=(temp*9/5)+32
+print("Celsius is {} and fahrenheit is {}".format(temp,f))
